2b21dda290d34ffdde8c68d6b52394c260d1b90a
Jeremy List <quick.dudley@gmail.com>
2016-09-26 21:44:58 +0800

    Haddocks for Phaser.ByteString

--- Phaser/ByteString.hs
+++ Phaser/ByteString.hs
@@ -11,10 +11,14 @@ import Control.Monad
 
 import Phaser.Core
 
+-- | A 'Phase' which takes 'BS.ByteString's as input and yields their individual
+-- bytes.
 unpackBS :: Phase p BS.ByteString Word8 ()
 unpackBS = (go >> unpackBS) <|> return () where
   go = get >>= BS.foldr (\w r -> yield w >> r) (return ())
 
+-- | A 'Phase' which takes lazy 'BL.ByteString's as input and yields their
+-- individual bytes.
 unpackLBS :: Phase p BL.ByteString Word8 ()
 unpackLBS = (go >> unpackLBS) <|> return () where
   go = get >>= BL.foldr (\w r -> yield w >> r) (return ())
