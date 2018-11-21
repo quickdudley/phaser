13614c8ae884379a2686a367b63fa1d992c72de7
Jeremy List <quick.dudley@gmail.com>
2018-11-22 11:29:15 +1300

    Make 'Alternative' instance for 'Permutable'

--- Codec/Phaser/Permutation.hs
+++ Codec/Phaser/Permutation.hs
@@ -12,15 +12,21 @@ data Permutable p c t a where
   Term :: Phase p c t a -> Permutable p c t a
   Filled :: a -> Permutable p c t a
   (:<*>) :: Permutable p c t (a -> b) -> Permutable p c t a -> Permutable p c t b
+  (:<|>) :: Permutable p c t a -> Permutable p c t a -> Permutable p c t a
 
 instance Functor (Permutable p c t) where
   fmap f (Term p) = Term (fmap f p)
   fmap f (Filled a) = Filled (f a)
   fmap f (l :<*> r) = (fmap . fmap) f l :<*> r
+  fmap f (l :<|> r) = fmap f l :<|> fmap f r
 
 instance Applicative (Permutable p c t) where
   pure = Filled
   (<*>) = (:<*>)
+
+instance (Monoid p) =>Alternative (Permutable p c t) where
+  empty = Term empty
+  (<|>) = (:<|>)
 
 -- | Create a 'Phase' which runs the constituent terms of the 'Permutable'
 -- in every order in which they succeed, running a separator 'Phase' between
@@ -33,10 +39,12 @@ runPermutable sep = go0 where
   resolve (Term p) = fromAutomaton $ starve $ toAutomaton p
   resolve (Filled a) = pure a
   resolve (l :<*> r) = resolve l <*> resolve r
+  resolve (l :<|> r) = resolve l <|> resolve r
   fill1 :: Permutable p c t x -> Phase p c t (Permutable p c t x)
   fill1 (Term p) = Filled <$> p
   fill1 (Filled _) = empty
   fill1 (l :<*> r) = (flip simplify r <$> fill1 l) <|> (simplify l <$> fill1 r)
+  fill1 (l :<|> r) = fill1 l <|> fill1 r
   simplify (Filled l) r = fmap l r
   simplify l  (Filled r) = fmap ($ r) l
   simplify l r = l :<*> r
